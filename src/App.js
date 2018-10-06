6c0572437e4f901b0cb46ace0043889e5fdfdbc7
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-10-06 00:32:02 -0400

    header and nav complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <section class="hero is-success">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">The Universe's Best Shopping Site</h1>
+            </div>
+          </div>
+        </section>
+        <div class="tabs is-centered">
+          <ul>
+            <li class="is-active">
+              <a>
+                <span class="icon is-small">
+                  <i class="fas fa-home" />{' '}
+                </span>
+                <span>TUBSS Home</span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span class="icon is-small">
+                  <i class="fas fa-male" />
+                </span>
+                <span>Men</span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span class="icon is-small">
+                  <i class="fas fa-female" />{' '}
+                </span>
+                <span>Women</span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span class="icon is-small">
+                  <i class="fas fa-paw" />{' '}
+                </span>
+                <span>Dog</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }
